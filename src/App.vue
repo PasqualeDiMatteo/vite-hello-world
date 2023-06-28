fd2f5bc51b69232f3552efafc507ee1c7647b3fc
PasqualeDiMatteo <pasqualedm94@gmail.com>
2023-06-28 14:23:55 +0200

    Added AppTitle

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <script>
+import AppTitle from "./components/AppTitle.vue"
 export default {
-
+    data() {
+        return {
+        }
+    },
+    components: {
+        AppTitle
+    }
 }
 </script>
 
-<template></template>
+<template>
+    <!-- Title -->
+    <AppTitle />
+</template>
 
 
 <style></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            title: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+
+<style>
+h1 {
+    text-align: center;
+    font-size: 3rem;
+}
+</style>
